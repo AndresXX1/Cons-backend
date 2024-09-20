5dfabf30d9d0abbf33ebec7d85edbf914caba097
alexismaz <alexisfajian@hotmail.com>
2024-09-20 12:52:05 -0300

    implemented

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -2,11 +2,14 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import * as DeviceDetector from 'device-detector-js';
-
+import * as qs from 'qs';
 @Injectable()
 export class ProductService {
   private readonly deviceDetector = new DeviceDetector();
   private readonly logger = new Logger(ProductService.name);
+  private finadiToken = '';
+  private argenToken = '';
+  private tokensExpires = new Date();
 
   constructor(private readonly configService: ConfigService) {}
 
@@ -42,4 +45,85 @@ export class ProductService {
     // }
     // console.log('resultProducts', resultProducts.data);
   }
+  async refreshTokens() {
+    try {
+      const newFinadiToken = await axios.post(
+        'https://rest.contabilium.com/token',
+        qs.stringify({
+          grant_type: 'client_credentials',
+          client_id: this.configService.get<string>('CLIENT_ID_FINADI'),
+          client_secret: this.configService.get<string>('CLIENT_SECRET_FINADI'),
+        }),
+        {
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+      const newArgenToken = await axios.post(
+        'https://rest.contabilium.com/token',
+        qs.stringify({
+          grant_type: 'client_credentials',
+          client_id: this.configService.get<string>('CLIENT_ID_ARGEN'),
+          client_secret: this.configService.get<string>('CLIENT_SECRET_ARGEN'),
+        }),
+        {
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+      this.argenToken = newArgenToken.data.access_token;
+      this.finadiToken = newFinadiToken.data.access_token;
+      const today = Math.floor(Date.now() / 1000);
+      this.tokensExpires = new Date((today + newFinadiToken.data.expires_in) * 1000);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+  validateTokenExpiration(): boolean {
+    const now = new Date();
+    return now < this.tokensExpires;
+  }
+
+  async getContabiliumProducts() {
+    try {
+      if (!this.validateTokenExpiration()) {
+        console.log('refreshing');
+        await this.refreshTokens();
+      }
+      const finadiProducts = await this.fetchAllProducts(this.finadiToken);
+      const argenProducts = await this.fetchAllProducts(this.argenToken);
+
+      const resultProducts = { finadi: finadiProducts, argen: argenProducts };
+      return resultProducts;
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+  fetchAllProducts = async (token: string) => {
+    let allProducts: any[] = [];
+    let page = 1;
+    let hasMore = true;
+
+    while (hasMore) {
+      const response = await axios.get(`https://rest.contabilium.com/api/conceptos/search?pageSize=0&page=${page}`, {
+        headers: {
+          authorization: `bearer ${token}`,
+          'content-Type': 'application/json',
+        },
+      });
+      allProducts = [...allProducts, ...response.data.Items];
+
+      if (response.data.TotalItems > page * 50) {
+        page++;
+      } else {
+        hasMore = false;
+      }
+    }
+
+    return allProducts;
+  };
 }
