1ee19e88b0529128b1f8fbc58b376696fa88c620
alexismaz <alexisfajian@hotmail.com>
2024-10-28 20:18:13 -0300

    implemented

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards, SetMetadata } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, SetMetadata, Get } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { META_ROLES } from '@infrastructure/constants';
@@ -13,6 +13,31 @@ export class NotificationController {
   @SetMetadata(META_ROLES, [RoleAdminType.ADMIN, RoleAdminType.SUPER_ADMIN])
   @Post('create')
   async createNotification(@Body() createNotificationDto: CreateNotificationDto) {
-    return this.notificationService.createNotification(createNotificationDto);
+    const result = await this.notificationService.createNotification(createNotificationDto);
+    return { ok: true, result };
+  }
+
+  @UseGuards(JwtAuthRolesGuard)
+  @SetMetadata(META_ROLES, [RoleAdminType.ADMIN, RoleAdminType.SUPER_ADMIN])
+  @Get('')
+  async getAllNotifications() {
+    const result = await this.notificationService.getAllNotifications();
+    return { ok: true, notifications: result };
+  }
+
+  @UseGuards(JwtAuthRolesGuard)
+  @SetMetadata(META_ROLES, [RoleAdminType.ADMIN, RoleAdminType.SUPER_ADMIN])
+  @Get('nextNotifications')
+  async getNextNotifications() {
+    const result = await this.notificationService.getNextNotifications();
+    return { ok: true, notifications: result };
+  }
+
+  @UseGuards(JwtAuthRolesGuard)
+  @SetMetadata(META_ROLES, [RoleAdminType.ADMIN, RoleAdminType.SUPER_ADMIN])
+  @Get('oldNotifications')
+  async getOldNotifications() {
+    const result = await this.notificationService.getOldNotifications();
+    return { ok: true, notifications: result };
   }
 }
